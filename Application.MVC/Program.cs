afb51482622904cac200b4d1356098635c8ca3f3
Steven-spec <reynasteven006@gmail.com>
2025-07-15 11:50:23 -0500

    avance

--- Application.MVC/Program.cs
+++ Application.MVC/Program.cs
@@ -54,7 +54,7 @@ namespace Application.MVC
             .AddRoles<Role>()
             .AddEntityFrameworkStores<AppDbContext>();
 
-            builder.Services.ConfigureApplicationCookie(options =>
+            builder.Services.ConfigureApplicationCookie(options => 
             {
                 options.LoginPath = "/Account/Login";
                 options.LogoutPath = "/Account/Logout";       
